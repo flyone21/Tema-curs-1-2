f15c0fba2c7f31118cf7f25077de693f969ab297
Sebastian Negrea <flyone21@gmail.com>
2023-10-19 22:49:30 +0300

    1

---
+++ src/Problema3_2.java
@@ -0,0 +1,13 @@
+package com.ItFactory;
+
+public class Problema3_2 {
+    public static void main(String[] args) {
+        double x = 3.2;
+        double y = 5.6;
+        double z = 2.3d;
+
+        double mediaAritmetica = (x+y+z) / 3.0;
+        System.out.println("Media aritmetica este: " + mediaAritmetica);
+
+    }
+}
